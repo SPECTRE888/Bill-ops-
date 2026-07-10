2ab91f7081e2f71829c384e0d6e2586dfe8bd2ef
SPECTRE888 <69078881+SPECTRE888@users.noreply.github.com>
2026-07-10 16:08:27 +0200

    Déplace le tarif kilométrique (0,30 €/km ou 0,50 €/km) dans le formulaire de facture (Mac et mobile) au lieu de Mon entreprise, choix conservé par facture (édition/duplication) + bump v1.0.43

--- mobile/sw.js
+++ mobile/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'billops-mobile-v3';
+const CACHE_NAME = 'billops-mobile-v4';
 const SHELL = ['./', 'index.html', 'manifest.json', 'apple-touch-icon.png', 'icon-192.png', 'icon-512.png'];
 
 self.addEventListener('install', (event) => {
